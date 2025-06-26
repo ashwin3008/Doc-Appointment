4bd4cc5128da971d87b6aa35fe39bfa67b440417
Ashwin <ashwinverma74@gmail.com>
2025-06-26 17:57:32 +0530

    dist folder added

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
