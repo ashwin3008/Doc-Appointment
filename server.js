ba3189b2a360e601a7bcba1b80c13d33c3721360
Ashwin <ashwinverma74@gmail.com>
2025-06-26 17:34:46 +0530

    updated development mode

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-const express =require('express')
-const colors= require('colors')
-const morgan =require('morgan')
-const dotenv =require('dotenv');
-const connectDB = require('./config/db');
-const cors=require('cors');
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
 dotenv.config();
 connectDB();
-
-const app=express()
+const path = require("path");
+const app = express();
 app.use(cors());
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan("dev"));
 colors.enable();
 
-app.use("/api/v1/user",require("./routes/userRoutes"));
-app.use("/api/v1/admin",require("./routes/adminRoutes"));
-app.use("/api/v1/doctor",require("./routes/doctorRoutes"));
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
-const port=process.env.PORT|| 3000;
-app.listen(port,()=>{
-    console.log(`server running in ${process.env.NODE_MODE} Mode on port${process.env.PORT}`.bgCyan.white
-    );
-    
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(
+    `server running in ${process.env.NODE_MODE} Mode on port${process.env.PORT}`
+      .bgCyan.white
+  );
 });
